0eef02092bb84826ab60b6d6e5d5cfd64394d342
Raul Menezes <raul.menezes@logicalglue.com>
2020-05-02 17:29:08 +0100

    01-first-chart - calculate chart dimensions

--- first-chart/chart.js
+++ first-chart/chart.js
@@ -1,6 +1,34 @@
 async function drawLineChart() {
   const dataset = await d3.json("../my_weather_data.json");
-  console.table(dataset[0])
+
+  const yAccessor = d => d.temperatureMax;
+
+  const dateParser = d3.timeParse("%Y-%m-%d");
+  const xAccessor = d => dateParser(d.date);
+
+  console.log(dataset[0]);
+  console.log(yAccessor(dataset[0]));
+  console.log(xAccessor(dataset[0]));
+
+  let dimensions = calculateChartDimensions();
+  console.log(dimensions);
+}
+
+function calculateChartDimensions() {
+  const dimensions = {
+    width: window.innerWidth * 0.9,
+    height: 400,
+    margin: {
+          top: 15,
+          right: 15,
+          bottom: 40,
+          left: 60,
+      },
+    }
+  
+    dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
+    dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
+    return dimensions; 
 }
 
 drawLineChart();
