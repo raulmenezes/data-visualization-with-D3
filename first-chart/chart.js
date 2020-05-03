9d120913436e8c93622ca403554c451184332562
Raul Menezes <raul.menezes@logicalglue.com>
2020-05-03 01:11:15 +0100

    01-first-chart - calculate chart scales

--- first-chart/chart.js
+++ first-chart/chart.js
@@ -1,17 +1,38 @@
 async function drawLineChart() {
   const dataset = await d3.json("../my_weather_data.json");
+  const dateParser = d3.timeParse("%Y-%m-%d");
 
   const yAccessor = d => d.temperatureMax;
-
-  const dateParser = d3.timeParse("%Y-%m-%d");
   const xAccessor = d => dateParser(d.date);
 
-  console.log(dataset[0]);
-  console.log(yAccessor(dataset[0]));
-  console.log(xAccessor(dataset[0]));
-
   let dimensions = calculateChartDimensions();
-  console.log(dimensions);
+
+  const wrapper = d3.select('#wrapper')
+    .append('svg')
+      .attr('width', dimensions.width)
+      .attr('height', dimensions.height)
+
+  const bounds = wrapper.append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${ dimensions.margin.top}px)`
+    )
+
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.boundedHeight, 0]);
+  
+  const freezingTemperaturePlacement = yScale(32)
+  const freezingTemperatures = bounds.append("rect")
+    .attr("x", 0)
+    .attr("width", dimensions.boundedWidth)
+    .attr("y", freezingTemperaturePlacement)
+    .attr("height", dimensions.boundedHeight- freezingTemperaturePlacement)
+    .attr("fill", "#e0f3f3")
+
+  const xScale = d3.scaleLinear()
+      .domain(d3.extent(dataset, xAccessor))
+      .range([0, dimensions.boundedWidth]);
 }
 
 function calculateChartDimensions() {
@@ -19,16 +40,16 @@ function calculateChartDimensions() {
     width: window.innerWidth * 0.9,
     height: 400,
     margin: {
-          top: 15,
-          right: 15,
-          bottom: 40,
-          left: 60,
-      },
-    }
-  
-    dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
-    dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
-    return dimensions; 
+      top: 15,
+      right: 15,
+      bottom: 40,
+      left: 60,
+    },
+  }
+
+  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
+  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
+  return dimensions;
 }
 
 drawLineChart();
